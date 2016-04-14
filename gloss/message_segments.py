bde310462d03735114048fdbf0e74d9a4abf7c1a
fredkingham <fredkingham@gmail.com>
2016-04-14 10:14:59 +0100

    using descriptors instead of lots of manual lookups, still needs status lookup lists key

--- gloss/message_segments.py
+++ gloss/message_segments.py
@@ -27,44 +27,84 @@ class Segment(object):
     def name(cls):
         return cls.__name__.upper()
 
+    def __init__(self, segment):
+        self.segment = segment
+
+
+class Hl7Field(object):
+    def __init__(self, *indexes, **kwargs):
+        self.indexes = indexes
+        self.required = kwargs.pop("required", True)
+
+    def __get__(self, obj, cls):
+        result = obj.segment
+
+        for i in self.indexes:
+            if not self.required:
+                if len(result) <= i or not result[i]:
+                    return None
+
+            result = result[i]
+
+        return result
+
+
+class DateTimeHl7Field(Hl7Field):
+    def __get__(self, *args):
+        result = super(DateTimeHl7Field, self).__get__(*args)
+
+        if not self.required and not result:
+            return result
+
+        return datetime.strptime(result, DATETIME_FORMAT)
+
+
+class DateHl7Field(Hl7Field):
+    def __get__(self, *args):
+        result = super(DateHl7Field, self).__get__(*args)
+
+        if not self.required and not result:
+            return result
+
+        return datetime.strptime(result, DATE_FORMAT).date()
+
 
 class MSH(Segment):
-    def __init__(self, segment):
-        self.trigger_event = segment[9][0][1][0]
-        self.message_type = segment[9][0][0][0]
-        # allergy information contains milli seconds, lets strip that
-        # off for the time being
+    trigger_event = Hl7Field(9, 0, 1, 0)
+    message_type = Hl7Field(9, 0, 0, 0)
+    message_datetime = DateTimeHl7Field(7, 0)
+    sending_application = Hl7Field(3, 0)
+    sending_facility = Hl7Field(4, 0)
 
-        self.message_datetime = datetime.strptime(segment[7][0][:12], DATETIME_FORMAT)
-        self.sending_application = segment[3][0]
-        self.sending_facility = segment[4][0]
+
+class MsaField(Hl7Field):
+    def __get__(self, *args):
+        result = super(MsaField, self).__get__(*args)
+        if result == "Call Successful":
+            return None
+        else:
+            return result
 
 
 class MSA(Segment):
-    def __init__(self, segment):
-        if not segment[3][0] == "Call Successful":
-            self.error_code = segment[3][0]
-        else:
-            self.error_code = None
+    error_code = MsaField(3, 0)
 
 
 class MRG(Segment):
-    def __init__(self, segment):
-        self.duplicate_hospital_number = segment[1][0][0][0]
+    duplicate_hospital_number = Hl7Field(1, 0, 0, 0)
 
 
 class ORC(Segment):
-    def __init__(self, segment):
-        pass
+    pass
 
 
 class UpdatePD1(Segment):
+
     @classmethod
     def name(cls):
         return "PD1"
 
-    def __init__(self, segment):
-        self.gp_practice_code = segment[4][1][0][0]
+    gp_practice_code = Hl7Field(4, 1, 0, 0)
 
 
 class QueryPD1(Segment):
@@ -72,8 +112,7 @@ class QueryPD1(Segment):
     def name(cls):
         return "PD1"
 
-    def __init__(self, segment):
-        self.gp_practice_code = segment[3][0][2][0]
+    gp_practice_code = Hl7Field(3, 0, 2, 0)
 
 
 class ResultsPID(Segment):
@@ -84,68 +123,51 @@ class ResultsPID(Segment):
     def name(cls):
         return "PID"
 
+    hospital_number = Hl7Field(3, 0, 0, 0)
+    surname = Hl7Field(5, 0, 0, 0)
+    first_name = Hl7Field(5, 0, 1, 0)
+    date_of_birth = DateHl7Field(7, 0, required=False)
+    gender = Hl7Field(8, 0)
+
     def __init__(self, segment):
+        super(ResultsPID, self).__init__(segment)
         self.nhs_number = segment[2][0]
         if isinstance(self.nhs_number, list):
             self.nhs_number = self.nhs_number[0][0]
-        self.hospital_number = segment[3][0][0][0]
-        self.surname = segment[5][0][0][0]
-        self.first_name = segment[5][0][1][0]
-        self.date_of_birth = None
-        if segment[7][0]:
-            self.date_of_birth = datetime.strptime(
-                segment[7][0][:8], DATE_FORMAT
-            ).date()
-        self.gender = segment[8][0]
+
 
 class InpatientPID(Segment):
     """
         the pid definition used by CARECAST systems
     """
+    hospital_number = Hl7Field(3, 0, 0, 0)
+    nhs_number = Hl7Field(3, 1, 0, 0, required=False)
+    surname = Hl7Field(5, 0, 0, 0)
+    first_name = Hl7Field(5, 0, 1, 0)
+    middle_name = Hl7Field(5, 0, 2, 0)
+    title = Hl7Field(5, 0, 4, 0)
+    date_of_birth = DateHl7Field(7, 0)
+
+    # this is used by spell delete
+    # it seems similar to our admission id
+    patient_account_number = Hl7Field(18, 0)
+    post_code = Hl7Field(11, 0, 4, 0)
+    date_of_death = DateHl7Field(29, 0, required=False)
 
     @classmethod
     def name(cls):
         return "PID"
 
     def __init__(self, segment):
-
-        self.hospital_number = segment[3][0][0][0]
-
-        if len(segment[3][1][0][0]):
-            self.nhs_number = segment[3][1][0][0]
-        else:
-            self.nhs_number = None
-
-        self.surname = segment[5][0][0][0]
-        self.first_name = segment[5][0][1][0]
-        self.middle_name = segment[5][0][2][0]
-        self.title = segment[5][0][4][0]
-        self.date_of_birth = datetime.strptime(
-            segment[7][0][:8], DATE_FORMAT
-        ).date()
-
+        super(InpatientPID, self).__init__(segment)
         self.sex = SEX_MAPPING.get(segment[8][0], None)
-
         self.religion = RELIGION_MAPPINGS.get(segment[17][0], None)
-
-        # this is used by spell delete
-        # it seems similar to our admission id
-        self.patient_account_number = segment[18][0]
         self.marital_status = MARITAL_STATUSES_MAPPING.get(
             segment[16][0], None
         )
         self.ethnicity = ETHNICITY_MAPPING.get(
             segment[22][0], None
         )
-
-        self.post_code = segment[11][0][4][0]
-
-        if len(segment[29][0]):
-            self.date_of_death = datetime.strptime(
-                segment[29][0], DATE_FORMAT
-            ).date()
-        else:
-            self.date_of_death = None
 
         self.death_indicator = None
         if len(segment[30][0]):
@@ -163,63 +185,49 @@ class AllergiesPID(Segment):
     def name(cls):
         return "PID"
 
-    def __init__(self, segment):
-        self.hospital_number = segment[3][0][0][0]
-        self.surname = segment[5][0][0][0]
-        self.first_name = segment[5][0][1][0]
-        self.date_of_birth = None
-        if segment[7][0]:
-            self.date_of_birth = datetime.strptime(
-                segment[7][0][:8], DATE_FORMAT
-            ).date()
-        self.gender = segment[8][0]
+    hospital_number = Hl7Field(3, 0, 0, 0)
+    surname = Hl7Field(5, 0, 0, 0)
+    first_name = Hl7Field(5, 0, 1, 0)
+    date_of_birth = DateHl7Field(7, 0, required=False)
 
-        # sample messages record 2 different types of
-        # message "No Known Allergies" and
-        # "Allergies Known and Recorde" we're
-        # querying as to whether there are others
-        self.allergy_status = segment[37][0]
+    # sample messages record 2 different types of
+    # message "No Known Allergies" and
+    # "Allergies Known and Recorde" we're
+    # querying as to whether there are others
+    allergy_status = Hl7Field(37, 0)
+
+
+    def __init__(self, segment):
+        super(AllergiesPID, self).__init__(segment)
+        # TODO this is wrong, it should probably be looking up the answer
+        self.gender = segment[8][0]
 
 
 class OBR(Segment):
+    lab_number = Hl7Field(3, 0)
+    profile_code = Hl7Field(4, 0, 0, 0)
+    profile_description = Hl7Field(4, 0, 1, 0)
+    request_datetime = DateTimeHl7Field(6, 0)
+    observation_datetime = DateTimeHl7Field(7, 0)
+    last_edited = DateTimeHl7Field(22, 0)
 
     def __init__(self, segment):
-        self.lab_number = segment[3][0]
-        self.profile_code = segment[4][0][0][0]
-        self.profile_description = segment[4][0][1][0]
-        self.request_datetime = datetime.strptime(
-            segment[6][0][:12], DATETIME_FORMAT
-        )
-        self.observation_datetime = datetime.strptime(
-            segment[7][0][:12], DATETIME_FORMAT
-        )
-
-        self.last_edited = datetime.strptime(
-            segment[22][0], DATETIME_FORMAT
-        )
-
+        super(OBR, self).__init__(segment)
         self.result_status = TEST_STATUS_MAPPING[segment[25][0]]
 
 
 
 class OBX(Segment):
+    value_type = Hl7Field(2, 0)
+    set_id = Hl7Field(1, 0)
+    test_name = Hl7Field(3, 0, 1, 0)
+    observation_value = Hl7Field(5, 0)
+    units = Hl7Field(6, 0, required=False)
+    reference_range = Hl7Field(7, 0, required=False)
+
     def __init__(self, segment):
-        self.value_type = segment[2][0]
-        self.set_id = segment[1][0]
+        super(OBX, self).__init__(segment)
         self.test_code = segment[3][0][0][0].replace("\\S\\", "^")
-        self.test_name = segment[3][0][1][0]
-        self.observation_value = segment[5][0]
-
-        if segment[6][0]:
-            self.units = segment[6][0]
-        else:
-            self.units = None
-
-        if len(segment) > 8 and segment[7][0]:
-            reference_range = segment[7][0]
-            self.reference_range = reference_range
-        else:
-            self.reference_range = None
 
         if len(segment) > 12 and segment[11][0]:
             self.result_status = OBX_STATUSES[segment[11][0]]
@@ -232,19 +240,10 @@ class OBX(Segment):
 
 
 class EVN(Segment):
-    def __init__(self, segment):
-        self.event_type = segment[1][0]
-        self.recorded_datetime = datetime.strptime(
-            segment[2][0][:12], DATETIME_FORMAT
-        )
-        planned_datetime = segment[3][0]
-
-        if planned_datetime:
-            self.planned_datetime = datetime.strptime(
-                planned_datetime[:12], DATETIME_FORMAT
-            )
-
-        self.event_description = segment[4][0]
+    event_type = Hl7Field(1, 0)
+    recorded_datetime = DateTimeHl7Field(2, 0)
+    planned_datetime = DateTimeHl7Field(3, 0)
+    event_description = Hl7Field(4, 0)
 
 
 class ResultsPV1(Segment):
@@ -252,82 +251,48 @@ class ResultsPV1(Segment):
     def name(cls):
         return "PV1"
 
-    def __init__(self, segment):
-        pass
-
 
 class PV1(Segment):
+    ward_code = Hl7Field(3, 0, 0, 0, required=False)
+    room_code = Hl7Field(3, 0, 1, 0, required=False)
+    bed_code = Hl7Field(3, 0, 2, 0, required=False)
+    datetime_of_admission = DateTimeHl7Field(44, 0)
+    datetime_of_discharge = DateTimeHl7Field(45, 0, required=False)
+
     def __init__(self, segment):
-        try:
-            self.ward_code = segment[3][0][0][0]
-        except IndexError:
-            self.ward_code = None
-        try:
-            self.room_code = segment[3][0][1][0]
-        except IndexError:
-            self.room_code = None
-
-        try:
-            self.bed_code = segment[3][0][2][0]
-        except IndexError:
-            self.bed_code = None
-
-        self.datetime_of_admission = datetime.strptime(
-            segment[44][0][:12], DATETIME_FORMAT
-        )
-
+        super(PV1, self).__init__(segment)
         try:
             self.admission_type = ADMISSION_TYPES[segment[2][0]]
         except:
             self.admission_types = None
 
-        if len(segment) > 45 and segment[45] and segment[45][0]:
-            self.datetime_of_discharge = datetime.strptime(
-                segment[45][0][:12], DATETIME_FORMAT
-            )
-        else:
-            self.datetime_of_discharge = None
-
 
 class PV2(Segment):
-    def __init__(self, segment):
-        self.admission_diagnosis = segment[3][0]
+    admission_diagnosis = Hl7Field(3, 0)
 
 
 class NTE(Segment):
-    def __init__(self, segment):
-        self.comments = segment[3][0]
-        self.set_id = segment[1][0]
+    comments = Hl7Field(3, 0)
+    set_id = Hl7Field(1, 0)
 
 
 class AL1(Segment):
+
     # there's a good chance we won't need
     # all these fields, but they
     # raise a lot of questions
-    def __init__(self, segments):
-        self.allergy_type = segments[2][0][0][0]
-        self.allergy_type_description = segments[2][0][1][0]
-        self.certainty_id = segments[2][0][3][0]
-        self.certainty_description = segments[2][0][4][0]
-        self.allergy_reference_name = segments[3][0][0][0]
-        self.allergy_description = segments[3][0][1][0]
-
-        self.allergen_reference_system = None
-        if len(segments[3][0]) > 2:
-            self.allergen_reference_system = segments[3][0][2][0]
-
-        self.allergen_reference = None
-        if len(segments[3][0]) > 3:
-            self.allergen_reference = segments[3][0][3][0]
-
-        self.status_id = segments[4][0][0][0]
-        self.status_description = segments[4][0][1][0]
-        self.diagnosis_datetime = datetime.strptime(
-            segments[4][0][4][0], DATETIME_FORMAT
-        )
-        self.allergy_start_datetime = datetime.strptime(
-            segments[6][0], DATETIME_FORMAT
-        )
+    allergy_type = Hl7Field(2, 0, 0, 0)
+    allergy_type_description = Hl7Field(2, 0, 1, 0)
+    certainty_id = Hl7Field(2, 0, 3, 0)
+    certainty_description = Hl7Field(2, 0, 4, 0)
+    allergy_reference_name = Hl7Field(3, 0, 0, 0)
+    allergy_description = Hl7Field(3, 0, 1, 0)
+    allergen_reference_system = Hl7Field(3, 0, 2, 0, required=False)
+    allergen_reference = Hl7Field(3, 0, 3, 0, required=False)
+    status_id = Hl7Field(4, 0, 0, 0)
+    status_description = Hl7Field(4, 0, 1, 0)
+    diagnosis_datetime = DateTimeHl7Field(4, 0, 4, 0)
+    allergy_start_datetime = DateTimeHl7Field(6, 0)
 
 
 class HL7Base(object):
