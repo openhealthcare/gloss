30b496e3acaebf7b10881d7a6ddf4747740dbddd
fredkingham <fredkingham@gmail.com>
2017-06-26 17:09:36 +0100

    instrumentation bug fixes

--- gloss/api/__init__.py
+++ gloss/api/__init__.py
@@ -64,6 +64,21 @@ def patient_query(issuing_source, identifier):
     return result.to_dict()
 
 
+@json_api('/api/patient_times/<identifier>', with_session=False)
+def patient_times(issuing_source, identifier):
+    info_source = get_information_source()
+    get_rows_start = time()
+    info_source.get_rows(identifier)
+    get_rows_time = time() - get_rows_start
+    get_patient_start = time()
+    info_source.patient_information(issuing_source, identifier)
+    get_patient_time = time() - get_patient_start
+    return {
+        "get_rows": get_rows_time,
+        "get_patient": get_patient_time
+    }
+
+
 @json_api('/api/result/<identifier>', with_session=False)
 def result_query(issuing_source, identifier):
     """ Returns all results for a patient
